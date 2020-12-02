e41453e2eac4f30d97edf1c591aa90824857ab7c
Librecz Bernadett <librecz.bernadett@magnetbank.hu>
2020-12-02 09:02:54 +0100

    Added more tests to day 1

--- src/test/kotlin/Puzzle1Test.kt
+++ src/test/kotlin/Puzzle1Test.kt
@@ -13,12 +13,31 @@ internal class Puzzle1Test {
         }
     }
 
+    @Test
+    fun `no 2 entries that sum to 2020`() {
+        val entries = arrayListOf(1, 2, 3)
+        val multiplicated = Puzzle1.find2elementsThatSumsUp2020AndMultiply(entries)
+        val expectedResult = -1
+        assertEquals(expectedResult, multiplicated) {
+            "$multiplicated should equal $expectedResult"
+        }
+    }
 
     @Test
     fun `find and multiply 3 entries that sum to 2020`() {
         val entries = arrayListOf(1721, 979, 366, 299, 675, 1456)
         val multiplicated = Puzzle1.find3elementsThatSumsUp2020AndMultiply(entries)
         val expectedResult = 241861950
+        assertEquals(expectedResult, multiplicated) {
+            "$multiplicated should equal $expectedResult"
+        }
+    }
+
+    @Test
+    fun `no 3 entries that sum to 2020`() {
+        val entries = arrayListOf(1, 2, 3)
+        val multiplicated = Puzzle1.find3elementsThatSumsUp2020AndMultiply(entries)
+        val expectedResult = -1
         assertEquals(expectedResult, multiplicated) {
             "$multiplicated should equal $expectedResult"
         }
